6754683591f30b5dae82b6e0d6b77de7ba8034a7
chegeras <chegeras@gmail>
2014-08-13 21:33:02 +0400

    ProductController added

---
+++ app/scripts/controller/ProductController.js
@@ -0,0 +1,12 @@
+/**
+ * Created by nikolay on 13.08.2014.
+ */
+(function(){
+    'use strict'
+    var ProductController = function(product){
+        this.product = product
+    };
+
+    ProductController.$inject = ['product'];
+    angular.module('auction').controller('ProductController',ProductController);
+});
